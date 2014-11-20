d8cf298a589b4a45fec928f8961544bb3eb506c3
Piotr Krzeminski <piotr@krzeminski.it>
2014-11-20 01:09:12 +0100

    More unit tests for the Python function evaluator class.

--- PythonFunctionEvaluator.py
+++ PythonFunctionEvaluator.py
@@ -1,20 +1,36 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'Piotr Krzemiński'
 
+import math
+
 
 class PythonFunctionEvaluator(object):
+    not_a_number = float("nan")
 
     def __init__(self):
         self.functionString = ""
+        self.arguments = {'x': lambda: 0}
 
     def validate(self):
         if self.functionString == "":
             return None
 
+        try:
+            self.__evaluate()
+        except SyntaxError as se:
+            return se
+
     def set_function(self, string):
         self.functionString = string;
 
-    def evaluate(self, xParam):
-        x = xParam
-        result = eval(self.functionString)
-        return result
+    def evaluate(self, x_param):
+        try:
+            self.arguments['x'] = x_param
+            result = self.__evaluate()
+            return result
+        except (ZeroDivisionError, ValueError):
+            return self.not_a_number
+
+    def __evaluate(self):
+            result = eval(self.functionString, globals(), self.arguments)
+            return result
--- Unit tests/PythonFunctionEvaluatorTests.py
+++ Unit tests/PythonFunctionEvaluatorTests.py
@@ -2,6 +2,7 @@
 __author__ = 'Piotr Krzemiński'
 
 import unittest
+import math
 from PythonFunctionEvaluator import PythonFunctionEvaluator
 
 
@@ -11,16 +12,59 @@ class PythonFunctionEvaluatorTests(unittest.TestCase):
 
     def test_validate_EmptyFunctionString_ShouldReturnNone(self):
         result = self.evaluator.validate()
+
         self.assertEquals(result, None)
 
     def test_validate_SimpleConstantFunction_ShouldReturnNone(self):
-        result = self.evaluator.set_function("3.5")
+        self.evaluator.set_function("3.5")
+        result = self.evaluator.validate()
+
         self.assertEquals(result, None)
 
-    def test_evaluate_SimpleConstantFunctionAndAnyArgument_ShouldReturnValue(self):
+    def test_validate_GenericSyntaxError_ShouldReturnInvalidSyntaxError(self):
+        self.evaluator.set_function("2.4 + * 3.5")
+        result = self.evaluator.validate()
+
+        expected_exception = SyntaxError()
+        expected_exception.lineno = 1
+        expected_exception.offset = 7
+        expected_exception.msg = "invalid syntax"
+
+        self.assertEquals(result.lineno, expected_exception.lineno, 'line number is invalid')
+        self.assertEquals(result.offset, expected_exception.offset, 'offset is invalid')
+        self.assertEquals(result.msg, expected_exception.msg, 'message is invalid')
+
+    # TODO
+    # def test_validate_NonexistentFunctionUsed_ShouldReturnNoSuchFunctionError(self):
+    #     self.evaluator.set_function("math.foobar(2)")
+    #     result = self.evaluator.validate()
+
+
+    def test_evaluate_SimpleConstantFunction_ShouldReturnValue(self):
         self.evaluator.set_function("3.5")
         result = self.evaluator.evaluate(53)
+
         self.assertEquals(result, 3.5)
+
+    def test_evaluate_LinearFunction_ShouldReturnValue(self):
+        self.evaluator.set_function("2*x + 3")
+        result = self.evaluator.evaluate(4)
+
+        self.assertEquals(result, 11)
+
+    def test_evaluate_DivisionByXEqualToZero_ShouldReturnNotANumber(self):
+        self.evaluator.set_function("1/x")
+        result = self.evaluator.evaluate(0)
+
+        self.assertTrue(math.isnan(result))
+
+    def test_evaluate_XIsBeyondAllowedRange_ShouldReturnNotANumber(self):
+        self.evaluator.set_function("math.log(x)")
+        result = self.evaluator.evaluate(-2)
+
+        self.assertTrue(math.isnan(result))
+
+
 
 if __name__ == '__main__':
     unittest.main()
