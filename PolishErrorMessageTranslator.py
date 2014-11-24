010eae77fe000fa90258fec97c09c6d7ff7fe6aa
Piotr Krzeminski <piotr@krzeminski.it>
2014-11-24 23:41:47 +0100

    More translations to Polish of error messages.

--- PolishErrorMessageTranslator.py
+++ PolishErrorMessageTranslator.py
@@ -21,6 +21,27 @@ def translate_to_polish(error):
         function_name = match.group(1)
         return "Błąd: nie wiem co to jest '" + function_name + "'!"
 
+    match = re.match(r"([^\(]+)\(\) takes exactly one argument \(([^\s]+) given\)", message, re.M | re.I)
+
+    if match:
+        function_name = match.group(1)
+        how_much_arguments_given = match.group(2)
+        return "Błąd: funkcja '" + function_name + "' przyjmuje 1 argument, a podano " + how_much_arguments_given + "!"
+
+    match = re.match(r"([^\s]+) expected ([^\s]+) arguments, got ([^\s]+)", message, re.M | re.I)
+
+    if match:
+        function_name = match.group(1)
+        how_much_arguments_expected = match.group(2)
+        how_much_arguments_given = match.group(3)
+        return "Błąd: funkcja '" + function_name + "' przyjmuje " + how_much_arguments_expected + " argumenty, a podano " + how_much_arguments_given + "!"
+
+    match = re.match(r"'([^']+)' object is not callable", message, re.M | re.I)
+
+    if match:
+        type_name = match.group(1)
+        return "Błąd: nie możesz wywołać obiektu/typu '" + type_name + "'!"
+
     if type(error) is SyntaxError:
         return "Błąd: coś jest nie tak ze składnią w linii " + str(error.lineno)\
                + ", przy znaku nr " + str(error.offset) + "!"
